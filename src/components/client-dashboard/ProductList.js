a4766b09b040d09019ba1a77260fba8aeba9a452
fish444556 <jialinl@mtu.edu>
2017-11-26 20:49:31 -0500

    added ProductList

---
+++ src/components/client-dashboard/ProductList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+class ProductList extends React.Component {
+	renderProductList() {
+		let list = [
+			{
+				'productname': 'nnnnnn',
+				'productdescriptionshort': 'dddddddddddd',
+				// 'sincedate': new Date()
+			},
+			{
+				'productname': 'n22222222',
+				'productdescriptionshort': 'd2222222222222',
+				// 'sincedate': new Date() + 1
+			}
+		]
+		debugger
+		return list.map((data) => {
+			return(
+				<li className='product-list-item'>
+					<Grid>
+						<Row>
+							<Col md={1}><span className="glyphicon glyphicon-barcode"></span></Col>
+		             		<Col md={2} className='product-list-item-middle'>
+		            			<div className="product-name"><b>{data.productname}</b></div>
+		               			<div className="description">{data.productdescriptionshort}</div>
+		               			<div className="since-date">2011-0923</div>
+		             		</Col>
+		             	 	<Col md={1} ><span className="glyphicon glyphicon-list"></span></Col>
+		           		</Row>
+		         	</Grid>
+		        </li>
+			)
+		})
+	}
+
+	render(){
+		return (
+			<div >
+				<ul>
+					{this.renderProductList()}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default ProductList;
--- src/components/client-dashboard/Products.js
+++ src/components/client-dashboard/Products.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import ProductList from './ProductList';
 
 class Products extends React.Component {
     render(){
         return (
-            <div><h1>Products</h1></div>
+            <div>
+              <h1>Products</h1>
+              <ProductList />
+            </div>
         );
     }
 };
